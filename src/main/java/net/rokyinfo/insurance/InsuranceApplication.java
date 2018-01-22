a6181a862d1cc46607d052f39ff8a59a4919acf8
caoyangyang <asd666490>
2018-01-22 14:37:33 +0800

    1.审核通过，写入订单激活时间和过期时间。 2.订单过期时间校验的定时任务。

--- src/main/java/net/rokyinfo/insurance/InsuranceApplication.java
+++ src/main/java/net/rokyinfo/insurance/InsuranceApplication.java
@@ -3,12 +3,13 @@ package net.rokyinfo.insurance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 /**
  * @author yuanzhijian
  */
 @SpringBootApplication
+@EnableScheduling
 public class InsuranceApplication {
 
     public static void main(String[] args) {
